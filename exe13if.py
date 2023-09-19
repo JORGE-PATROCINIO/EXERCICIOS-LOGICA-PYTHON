1c6ec1662b532a5b71b993c01bd4061b7ad2071a
JORGE PATROCÍNIO <103794898+JORGE-PATROCINIO@users.noreply.github.com>
2023-09-18 22:40:29 -0300

    Update exe13if.py

--- exe13if.py
+++ exe13if.py
@@ -2,7 +2,8 @@
 # DEV: JORGE AUGUSTO
 # DATA: 13/09/2022
 
-#Tendo como entrada a altura e o sexo (codificado da seguinte forma: 1: feminino 2: masculino) de uma pessoa, construa um programa que calcule e imprima seu peso ideal, utilizando as seguintes Fórmulas:
+#Tendo como entrada a altura e o sexo (codificado da seguinte forma: 1: feminino 2: masculino) de uma pessoa,
+#construa um programa que calcule e imprima seu peso ideal, utilizando as seguintes Fórmulas:
 
 #para homens: (72.7 * Altura) – 58
 #para mulheres: (62.1 * Altura) – 44.7
