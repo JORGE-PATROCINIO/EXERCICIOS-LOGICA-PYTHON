76832fadb72095d6373c9336253fd561d28ac0a0
JORGE PATROCÍNIO <103794898+JORGE-PATROCINIO@users.noreply.github.com>
2023-09-18 22:25:40 -0300

    Update exe09if.py

--- exe09if.py
+++ exe09if.py
@@ -2,7 +2,8 @@
 # DEV: JORGE AUGUSTO
 # DATA: 13/09/2022
 
-#Escreva um programa para ler o ano de nascimento de uma pessoa e escrever uma mensagem que diga se ela poderá ou não votar este ano (não é necessário considerar o mês em que ela nasceu).
+#Escreva um programa para ler o ano de nascimento de uma pessoa e escrever uma mensagem que diga se ela poderá
+#ou não votar este ano (não é necessário considerar o mês em que ela nasceu).
 
 anoatual = int(input("Digite o ano atual com 4 digítos: "))
 anonascimento = int(input("Digite o ano de nascimento com 4 digítos: "))
