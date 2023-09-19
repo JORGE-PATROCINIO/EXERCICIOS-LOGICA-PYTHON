f9bd19fb835d229545e7b8d7e61f6a90ec85a664
JORGE PATROCÍNIO <103794898+JORGE-PATROCINIO@users.noreply.github.com>
2023-09-18 22:28:55 -0300

    Update exe10if.py

--- exe10if.py
+++ exe10if.py
@@ -2,10 +2,11 @@
 # DEV: JORGE AUGUSTO
 # DATA:12/09/2022
 
-# Escreva um programa que verifique a validade de uma senha fornecida pelo usuário. A senha válida é o número 1234. Devem ser impressas as seguintes mensagens: ACESSO PERMITIDO caso a senha seja válida e  ACESSO NEGADO caso a senha seja inválida.
+# Escreva um programa que verifique a validade de uma senha fornecida pelo usuário. A senha válida é o número 1234.
+#Devem ser impressas as seguintes mensagens: ACESSO PERMITIDO caso a senha seja válida e  ACESSO NEGADO caso a senha seja inválida.
 
 senha = 1234
-for i in range(3):
+for i in range(0,3,1):
     x =int(input("Digite a senha: "))
     if x != senha:
         if i == 2:
