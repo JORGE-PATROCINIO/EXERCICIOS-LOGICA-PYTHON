6954e524daaffb0fa7876f02382cee3c56268a2a
JORGE PATROCÍNIO <103794898+JORGE-PATROCINIO@users.noreply.github.com>
2023-09-18 21:18:56 -0300

    Update exe04if.py

--- exe04if.py
+++ exe04if.py
@@ -2,7 +2,9 @@
 # Dev: Jorge Augusto
 # 30/08/2022
 
-#Faça um programa que receba o salário bruto de um funcionário e, usando a tabela a seguir, calcule e mostre o valor a receber. Sabe-se que este é composto pelo salário bruto acrescido de gratificação e descontado o imposto de 7% sobre o salário.
+#Faça um programa que receba o salário bruto de um funcionário e,
+#usando a tabela a seguir, calcule e mostre o valor a receber.
+#Sabe-se que este é composto pelo salário bruto acrescido de gratificação e descontado o imposto de 7% sobre o salário.
 
 #         SALÁRIO                          GRATIFICAÇÃO
 #       ATÉ R$ 350,00                        R$ 100,00
