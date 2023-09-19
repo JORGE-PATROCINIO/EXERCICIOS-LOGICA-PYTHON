15aae78bfd152b16efc5d4bcebbd374eecae9022
JORGE PATROCÍNIO <103794898+JORGE-PATROCINIO@users.noreply.github.com>
2023-09-18 22:34:49 -0300

    Update exe11if.py

--- exe11if.py
+++ exe11if.py
@@ -2,7 +2,8 @@
 # DEV: JORGE AUGUSTO
 # DATA:13/09/2022
 
-#As maçãs custam R$ 0,30 cada se forem compradas menos do que uma dúzia, e R$ 0,25 se forem compradas pelo menos doze. Escreva um programa que leia o número de maçãs compradas, calcule e escreva o valor total da compra.
+#As maçãs custam R$ 0,30 cada se forem compradas menos do que uma dúzia, e R$ 0,25 se forem compradas pelo menos doze.
+#Escreva um programa que leia o número de maçãs compradas, calcule e escreva o valor total da compra.
 
 N = int(input("Digite a quantidade de maçãs: "))
 
