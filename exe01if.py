a3006f8d75d7520915cd5da04112c6e3f4c127af
JORGE PATROCÍNIO <103794898+JORGE-PATROCINIO@users.noreply.github.com>
2023-09-18 21:17:16 -0300

    Update exe01if.py

--- exe01if.py
+++ exe01if.py
@@ -2,15 +2,17 @@
 # DEV: JORGE AUGUSTO
 # DATA: 30/08/2022
 
-#Uma empresa decidiu dar uma gratificação de Natal a seus funcionários, baseada no número de horas extras e no número de horas que o funcionário faltou ao trabalho. O valor do prêmio é obtido pela consulta à tabela que se segue, na qual:
+#Uma empresa decidiu dar uma gratificação de Natal a seus funcionários, 
+#baseada no número de horas extras e no número de horas que o funcionário faltou ao trabalho.
+#O valor do prêmio é obtido pela consulta à tabela que se segue, na qual:
 
 # H = número de horas extras – (2/3 * ( número de horas falta ))         H (MINUTOS	PRÊMIO (R$)
 
-#                       >= 2.401	                                             500,00
-#                      1.801 até 2.400                                           400,00
-#                      1.201 até 1.800                                           300,00
-#                        600 até 1.200                                           200,00
-#                      <600	                                                     100,00
+#                      >= 2.401	                                             500,00
+#                      1.801 até 2.400                                       400,00
+#                      1.201 até 1.800                                       300,00
+#                      600 até 1.200                                         200,00
+#                      <600	                                                 100,00
 rep = True
 while rep:
     horaextra = int(input("Digite o número de horas extras: "))
