6b1566d7f7edb58ea459727868561a282181d6ef
JORGE PATROCÍNIO <103794898+JORGE-PATROCINIO@users.noreply.github.com>
2023-09-18 22:26:20 -0300

    Update exe10if.1.py

--- exe10if.1.py
+++ exe10if.1.py
@@ -2,7 +2,8 @@
 # DEV: JORGE AUGUSTO
 # DATA:12/09/2022
 
-# Escreva um programa que verifique a validade de uma senha fornecida pelo usuário. A senha válida é o número 1234. Devem ser impressas as seguintes mensagens: ACESSO PERMITIDO caso a senha seja válida e  ACESSO NEGADO caso a senha seja inválida.
+# Escreva um programa que verifique a validade de uma senha fornecida pelo usuário. A senha válida é o número 1234.
+#Devem ser impressas as seguintes mensagens: ACESSO PERMITIDO caso a senha seja válida e  ACESSO NEGADO caso a senha seja inválida.
 
 
 senha = 1234
