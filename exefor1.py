ceddac852ce08c4730c0baaaafeb732538768d06
JORGE PATROCÍNIO <103794898+JORGE-PATROCINIO@users.noreply.github.com>
2023-09-18 22:58:46 -0300

    Update exefor1.py

--- exefor1.py
+++ exefor1.py
@@ -2,6 +2,16 @@
 # DEV: JORGE AUGUSTO
 # DATA: 13/09/2022
 
+# Um funcionário de uma empresa recebe, anualmente, aumento salarial. Sabe-se que:
+
+# a) esse funcionário foi contratado em 2000, com salário inicial de R$1.000,00;
+
+# b) Em 2001, ele recebeu aumento de 1,5%, sobre o seu salário inicial;
+
+# c) A partir de 2002 (inclusive), os aumentos salariais sempre corresponderam ao dobro do percentual do ano anterior.
+
+# Faça um programa que determine o salário desse funcionário dos anos de 2000 à 2017. Apresente todos os valores.
+
 SI = TAXA = SA = REAJ = 0
 
 for ano in range(2000, 2018, 1):
