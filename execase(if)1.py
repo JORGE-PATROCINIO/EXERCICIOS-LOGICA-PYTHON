eb75cab5cea0a25b1861d5632591e9ce67df106c
JORGE PATROCÍNIO <103794898+JORGE-PATROCINIO@users.noreply.github.com>
2023-09-18 22:51:33 -0300

    Update execase(if)1.py

--- execase(if)1.py
+++ execase(if)1.py
@@ -2,6 +2,34 @@
 # DEV: JORGE AUGUSTO
 # DATA:18/09/2022
 
+#Faça um programa que receba o preço, a categoria (1 – limpeza; 2 – alimentação; ou 3 – vestuário) e
+#a situação (R – produtos que necessitam de refrigeração;
+#e N – produtos que não necessitam de refrigeração). Calcule e mostre:
+
+#O valor do aumento, usando as regras que se seguem.
+
+#  PREÇO         CATEGORIA          PERCENTUAL DE AUMENTO
+#  <=25              1                       5%
+#  <=25              2                       8%
+#  <=25              3                       10%
+#  >25               1                       12%
+#  >25               2                       15%
+#  >25               3                       18%
+
+#O valor do imposto, usando as regras a seguir.
+
+#O produto que preencher pelo menos um dos seguintes requisitos pagará imposto equivalente a 5% do preço; caso contrário, pagará 8%. Os requisitos são:
+#Categoria: 2
+#Situação: R
+
+#O novo preço, ou seja, o preço mais aumento menos imposto.
+#A classificação, usando as regras a seguir.
+
+#  NOVO PREÇO             CLASSIFICAÇÃO
+# <= 50                      BARATO
+# ENTRE 50 E 120             NORMAL
+# > 120                      CARO
+
 imposto = 0.08
 preco = float(input("Digite o preço: "))
 categoria = int(input("Digite a categoria:"))
